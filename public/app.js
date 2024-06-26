f4a39ef46fbb5728eae1192487f296209566c18c
vidwan <163408880+vidwanns@users.noreply.github.com>
2024-06-26 17:30:44 +0530

    info of images

--- public/app.js
+++ public/app.js
@@ -6,11 +6,11 @@ let carouselDom = document.querySelector('.carousel');
 let SliderDom = carouselDom.querySelector('.carousel .list');
 let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
 let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-let timeDom = document.querySelector('.carousel .time');
+// let timeDom = document.querySelector('.carousel .time');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
 let timeRunning = 3000;
-let timeAutoNext = 7000;
+// let timeAutoNext = 7000;
 
 nextDom.onclick = function(){
     showSlider('next');    
